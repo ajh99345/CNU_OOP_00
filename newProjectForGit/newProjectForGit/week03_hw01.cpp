7d179f3de8f012b5efe3167066ec1904c2f900fb
Jun <ajh99345@naver.com>
2020-09-20 17:48:35 +0900

    week03

---
+++ newProjectForGit/newProjectForGit/week03_hw01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+class SetValue {
+	int x, y;
+public:
+	void xsetter(int value);
+	void ysetter(int value);
+	int xgetter();
+	int ygetter();
+};
+
+void SetValue::xsetter(int value) {
+	x = value;
+}
+
+void SetValue::ysetter(int value) {
+	y = value;
+}
+
+int SetValue::xgetter() {
+	return x;
+}
+
+int SetValue::ygetter() {
+	return y;
+}
+
+int main() {
+	SetValue obj;
+	obj.xsetter(33);
+	obj.ysetter(44);
+
+	std::cout << "X= " << obj.xgetter() << " , Y= " << obj.ygetter() << std::endl;
+	system("pause");
+	return 0;
+}
