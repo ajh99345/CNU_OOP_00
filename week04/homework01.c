f1dcdb61f0b55e3cdec5c4d92cfa54da2986f53c
Jun <ajh99345@naver.com>
2020-09-27 16:33:15 +0900

    week04 09.27

--- week04/homework01.c
+++ week04/homework01.c
@@ -3,9 +3,9 @@
 
 int main() {
 	char arr[101];
-	printf("문자를 입력하세요(100자 이내).");
+	printf("문자를 입력하세요(100자 이내).\n");
 	scanf("%s", arr);
-	printf("%s", arr);
+	printf("입력된 문자는 %s 입니다.", arr);
 
 	return 0;
 }
